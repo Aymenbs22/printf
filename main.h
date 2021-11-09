82572b8c65c0327e39f33947ecfb3192e530b10e
Aymenbs22 <aymenbenslima0@gmail.com>
2021-11-09 11:09:58 -0800

    main

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <limits.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+int _putchar(char c);
+int _printf(char *format, ...);
+int print_int(int n);
+int print_char(char *ch);
+int print_string(char *ch);
+#endif
