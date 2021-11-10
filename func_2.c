181a6cd5d78898311c120146785cdb876a825952
Aymenbs22 <aymenbenslima0@gmail.com>
2021-11-10 05:44:23 -0800

    function

---
+++ func_2.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_percent - print percent
+ *@arg: argument of type of va_list
+ * Return: integer
+ */
+int print_percent(va_list arg)
+{
+	(void)arg;
+
+	_putchar('%');
+	return (1);
+}
